34b21105fde75b5e0a13d7b9e59e8c6a628e6098
Sean-XZW <113716662+Sean-XZW@users.noreply.github.com>
2022-09-17 19:51:56 +0800

    Add files via upload

---
+++ Fibonacci recurrsion.py
@@ -0,0 +1,11 @@
+x = int(input("pls enter a number"))
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+for i in range(0, x):
+    print(fibonacci(i))
